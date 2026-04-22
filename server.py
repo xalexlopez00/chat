33959fcb2403f204d7ba6ca37b4f65653cdfe588
xalexlopez00 <xalexl.opez00@gmail.com>
2026-04-22 11:31:24 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,28 +8,28 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
-ROOM_PASSWORDS = {"general": ""} 
-AUTHORIZED_USERS = {} # { sid: [lista_salas_autorizadas] }
+# ROOM_DATA almacena: { 'nombre': {'pass': '...', 'temp': True/False} }
+ROOM_DATA = {"general": {"pass": "", "temp": False}} 
+AUTHORIZED_USERS = {} 
 
 @socketio.on('connect')
 def handle_connect():
     AUTHORIZED_USERS[request.sid] = ["general"]
     join_room("general")
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    if request.sid in AUTHORIZED_USERS: del AUTHORIZED_USERS[request.sid]
-
 @socketio.on('create_room')
 def handle_create(data):
     room = data.get('room').lower()
     password = data.get('password', '')
+    is_temp = data.get('temp', False) 
+    
     if room:
-        ROOM_PASSWORDS[room] = password
+        ROOM_DATA[room] = {"pass": password, "temp": is_temp}
         if request.sid in AUTHORIZED_USERS: AUTHORIZED_USERS[request.sid].append(room)
         join_room(room)
-        emit('join_success', {'room': room})
-        emit('new_room_available', {'room': room}, broadcast=True)
+        # Confirmamos al creador y avisamos al resto
+        emit('join_success', {'room': room, 'temp': is_temp})
+        emit('new_room_available', {'room': room, 'has_pass': password != ""}, broadcast=True)
 
 @socketio.on('join')
 def handle_join(data):
@@ -37,29 +37,21 @@ def handle_join(data):
     password = data.get('password', '')
     nickname = data.get('nickname', '')
     
-    es_admin = (nickname == "Admin")
-    
-    if room in ROOM_PASSWORDS and ROOM_PASSWORDS[room] != "" and not es_admin:
-        if ROOM_PASSWORDS[room] != password:
+    info = ROOM_DATA.get(room, {"pass": "", "temp": False})
+    if info["pass"] != "" and nickname != "Admin":
+        if info["pass"] != password:
             emit('error_msg', {'msg': "Contraseña incorrecta"})
             return
 
     join_room(room)
     if room not in AUTHORIZED_USERS[request.sid]:
         AUTHORIZED_USERS[request.sid].append(room)
-    emit('join_success', {'room': room})
-
-@socketio.on('leave')
-def handle_leave(data):
-    room = data.get('room')
-    if room:
-        leave_room(room)
+    emit('join_success', {'room': room, 'temp': info["temp"]})
 
 @socketio.on('message')
 def handle_message(data):
     room = data.get('room', 'general')
     msg = data.get('msg')
-    # IMPORTANTE: El mensaje solo se emite a 'room=room'
     if room in AUTHORIZED_USERS.get(request.sid, []):
         emit('message', {'msg': msg, 'room': room}, room=room, include_self=False)
 
