35d40df7ee0e07bbd183a2dcf9e31163331efe5e
xalexlopez00 <xalexl.opez00@gmail.com>
2026-04-23 09:50:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from gevent import monkey
 monkey.patch_all()
 import os
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
@@ -10,8 +10,18 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 # Almacén de salas
 ROOMS = {"general": {"history": [], "temp": False, "pass": "", "users": set()}}
 
+# Ruta para el icono en la web (Favicon)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 def sync_rooms():
-    data = {n: {"temp": i["temp"], "locked": bool(i["pass"]), "count": len(i["users"])} for n, i in ROOMS.items()}
+    data = {n: {
+        "temp": i["temp"], 
+        "locked": bool(i["pass"]), 
+        "count": len(i["users"])
+    } for n, i in ROOMS.items()}
     socketio.emit('update_rooms', data)
 
 @socketio.on('register_user')
@@ -25,29 +35,41 @@ def handle_reg(data):
 def handle_create(data):
     name = data.get('room', '').lower().strip()
     if name and name not in ROOMS:
-        ROOMS[name] = {"history": [], "temp": data.get('temp', False), "pass": data.get('password', ""), "users": set()}
+        ROOMS[name] = {
+            "history": [], 
+            "temp": data.get('temp', False), 
+            "pass": data.get('password', ""), 
+            "users": set()
+        }
         sync_rooms()
 
 @socketio.on('join')
 def handle_join(data):
-    room, pw, old = data.get('room'), data.get('password', ""), data.get('old_room')
+    room = data.get('room')
+    pw = data.get('password', "")
+    old_room = data.get('old_room')
+    
     if room in ROOMS:
         if ROOMS[room]["pass"] and pw != ROOMS[room]["pass"]:
             emit('error_msg', {'msg': "🔒 Contraseña incorrecta"})
             return
-        if old in ROOMS:
-            leave_room(old)
-            if request.sid in ROOMS[old]["users"]: ROOMS[old]["users"].remove(request.sid)
+            
+        if old_room and old_room in ROOMS:
+            leave_room(old_room)
+            if request.sid in ROOMS[old_room]["users"]:
+                ROOMS[old_room]["users"].remove(request.sid)
+        
         join_room(room)
         ROOMS[room]["users"].add(request.sid)
         sync_rooms()
-        emit('room_joined', {'room': room, 'history': [] if ROOMS[room]["temp"] else ROOMS[room]["history"]})
+        hist = [] if ROOMS[room]["temp"] else ROOMS[room]["history"]
+        emit('room_joined', {'room': room, 'history': hist})
 
 @socketio.on('message')
 def handle_msg(data):
     room = data.get('room')
     if room in ROOMS:
-        if not ROOMS[room]['temp']: 
+        if not ROOMS[room]['temp']:
             ROOMS[room]['history'].append(data)
         emit('new_message', data, room=room, include_self=False)
 
